8a227676228dcd14dd8257c1aa8c8dc97915252e
emanuelegoldin <emanuele.goldin@gmail.com>
2026-02-18 16:26:46 +0000

    Reduce image size for nextjs-app from 3.46 GB to 435 MB

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone',
   async rewrites() {
     const backendUrl = process.env.BACKEND_URL;
     if (!backendUrl) return [];
